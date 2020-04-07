bf943cff2fe7c1eb20a3bcd20d001b62e57fd7cc
modern-fork-2657 <kalkidan.solomon2@gmail.com>
2020-04-07 00:18:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function updateObjectWithKeyAndValue(object,key,value){
   let newObj= Object.assign({},object,{[key]:value});
   return newObj;
-}
+};
+
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
+  object.key=value;
+  return object;
+};
