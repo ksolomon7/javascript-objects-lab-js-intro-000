0f13951b868372bb9a898d0e1d227dd0c303acd4
modern-fork-2657 <kalkidan.solomon2@gmail.com>
2020-04-07 00:26:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ function updateObjectWithKeyAndValue(object,key,value){
 };
 
 function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
-  object.key=value;
-  return object;
+  return object.key=value;
 };
 
 function deleteFromObjectByKey(object,key){
